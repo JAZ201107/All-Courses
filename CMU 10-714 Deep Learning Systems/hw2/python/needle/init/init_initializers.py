1e2d0b95315801e9400962082e324388440169d8
zhang yuyang <zhangyuyang1211@gmail.com>
2024-04-24 17:24:28 +0800

    CMU Deep Learning System HW 0 1 2 3 Finished

---
+++ CMU 10-714 Deep Learning Systems/hw2/python/needle/init/init_initializers.py
@@ -0,0 +1,36 @@
+import math
+from .init_basic import *
+
+
+def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
+    ### BEGIN YOUR SOLUTION
+    # raise NotImplementedError()
+    alpha = gain * math.sqrt(6 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-alpha, high=alpha, **kwargs)
+    ### END YOUR SOLUTION
+
+
+def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
+    ### BEGIN YOUR SOLUTION
+    # raise NotImplementedError()
+    std = gain * math.sqrt(2 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0, std=std, **kwargs)
+    ### END YOUR SOLUTION
+
+
+def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
+    assert nonlinearity == "relu", "Only relu supported currently"
+    ### BEGIN YOUR SOLUTION
+    # raise NotImplementedError()
+    bound = math.sqrt(6 / fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
+    ### END YOUR SOLUTION
+
+
+def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
+    assert nonlinearity == "relu", "Only relu supported currently"
+    ### BEGIN YOUR SOLUTION
+    # raise NotImplementedError()
+    std = math.sqrt(2 / fan_in)
+    return randn(fan_in, fan_out, mean=0, std=std, **kwargs)
+    ### END YOUR SOLUTION
